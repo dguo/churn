8ecad7d18c18dde1d59569bdaf5e6349cabfab6f
Danny Guo <dannyguo91@gmail.com>
2017-01-16 18:06:41 -0500

    Clean up reward-types implementation

--- src/subcommands/reward_types.py
+++ src/subcommands/reward_types.py
@@ -2,8 +2,9 @@ import sqlite3
 
 import click
 from pick import pick
+from tabulate import tabulate
 
-from ..util import pick_with_cancel
+from ..util import pick_with_cancel, successful_update_message
 
 def _get_reward_types(connection):
     command = 'SELECT id, description FROM reward_types ORDER BY description'
@@ -17,10 +18,10 @@ def select_reward_type_id(connection):
     return reward_types[selection[1]]['id']
 
 def list_reward_types(connection):
-    click.echo_via_pager('\n'.join(
-        [reward_type['description']
-         for reward_type in _get_reward_types(connection)]
-    ))
+    reward_types = [[index + 1, reward_type['description']]
+                    for index, reward_type
+                    in enumerate(_get_reward_types(connection))]
+    click.echo_via_pager(tabulate(reward_types, tablefmt='fancy_grid'))
 
 def add_reward_type(connection):
     new_reward_type = click.prompt('Please enter a new reward type')
@@ -43,10 +44,13 @@ def remove_reward_type(connection):
         return
     selection = pick_with_cancel(title, reward_types)
     if selection:
+        if not click.confirm('Remove ' + selection[0] +
+                             ' and all of its related rewards?'):
+            return
         command = 'DELETE FROM reward_types WHERE description = ?'
         with connection:
             connection.execute(command, (selection[0],))
-        click.echo('Removed the reward type: ' + selection[0])
+        click.secho('Removed the reward type: ' + selection[0], fg='green')
 
 def update_reward_type(connection):
     title = 'Please select a reward type.'
@@ -57,8 +61,9 @@ def update_reward_type(connection):
         return
     selection = pick_with_cancel(title, reward_types)
     if selection:
-        new_reward_type = click.prompt('Please enter a new reward type')
+        new_reward_type = click.prompt('Please enter a new description')
         command = ('UPDATE reward_types SET description = ? '
                    'WHERE description = ?')
         with connection:
             connection.execute(command, (new_reward_type, selection[0]))
+        successful_update_message()
